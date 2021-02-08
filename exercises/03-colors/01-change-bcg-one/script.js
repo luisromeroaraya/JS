da497974c157e704a1f51cfdcfa5dd74f8dd2a0b
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-08 14:04:11 +0100

    Add 01-change-bcg-one

--- exercises/03-colors/01-change-bcg-one/script.js
+++ exercises/03-colors/01-change-bcg-one/script.js
@@ -10,5 +10,20 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("red").addEventListener("click", () => {
+        document.body.style.backgroundColor = "red";        
+    });
+
+    document.getElementById("green").addEventListener("click", () => {
+        document.body.style.backgroundColor = "green";
+    });
+
+    document.getElementById("yellow").addEventListener("click", () => {
+        document.body.style.backgroundColor = "yellow";
+    });
+
+    document.getElementById("blue").addEventListener("click", () => {
+        document.body.style.backgroundColor = "blue";    
+    });
+
 })();
