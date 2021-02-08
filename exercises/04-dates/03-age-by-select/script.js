4538098937cd1a0c221552cac37def72a46c9bfe
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-08 16:03:40 +0100

    Add 03-age-by-select

--- exercises/04-dates/03-age-by-select/script.js
+++ exercises/04-dates/03-age-by-select/script.js
@@ -10,5 +10,10 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        var nacimiento = new Date(document.getElementById("dob-year").value, document.getElementById("dob-month").value, document.getElementById("dob-day").value)
+        var hoy = new Date();
+        var edad = Math.floor((hoy - nacimiento) / 86400000 / 365);
+        alert("Vous avez: "+ edad +" ans"); 
+    });
 })();
