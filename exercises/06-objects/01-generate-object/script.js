bbd564f40383b52695dacf2f3c39a26b8f48a047
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-10 09:34:28 +0100

    Add 01-generate-object

--- exercises/06-objects/01-generate-object/script.js
+++ exercises/06-objects/01-generate-object/script.js
@@ -10,5 +10,10 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    document.getElementById("run").addEventListener("click", () => {
+        const object = {lastname: "", firstname: "", age: "", city: "", country: ""};
+        console.log(object);
+    });
+
 })();
