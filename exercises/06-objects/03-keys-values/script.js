cb3c27fcc9bf64c6c7de26e9cebb79e9bd322206
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-10 11:44:29 +0100

    Add 03-keys-values

--- exercises/06-objects/03-keys-values/script.js
+++ exercises/06-objects/03-keys-values/script.js
@@ -18,5 +18,10 @@
         city: "Liège",
         status: "married",
     };
-    // your code here
+
+    document.getElementById("run").addEventListener("click", () => {
+        console.log(Object.keys(person));
+        console.log(Object.values(person));
+    });
+
 })();
