b1e9e9c5b0d2c0e0dc21d9b4dbda789e7343a51a
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-10 14:16:49 +0100

    Add 01-instances

--- exercises/07-classes/01-instances/script.js
+++ exercises/07-classes/01-instances/script.js
@@ -16,5 +16,10 @@
             this.age = age;
         }
     }
-    // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        const skitty = new Cat("Skitty",9);
+        const pixel = new Cat("Pixel",6);
+        console.log(skitty);
+        console.log(pixel);
+    });
 })();
