1b9ed193167d7d5cd260aed3df3c98b63097e874
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-10 15:09:11 +0100

    Add 03-inheritance

--- exercises/07-classes/03-inheritance/script.js
+++ exercises/07-classes/03-inheritance/script.js
@@ -15,5 +15,29 @@
             return `${this.constructor.greeting}! I'm ${this.name}!`;
         }
     }
-    // your code here
+
+    class Cat extends Animal {
+        constructor(name) {
+            super();
+            this.name = name;
+        }
+        static greeting = "Miaow";
+    }
+
+    class Dog extends Animal {
+        constructor(name) {
+            super();
+            this.name = name;
+        }
+        static greeting = "Wouf";
+    }
+
+
+    document.getElementById("run").addEventListener("click", () => {
+        const perro = new Dog(prompt("Please enter your dog's Name:"));
+        const gato = new Cat(prompt("Please enter your cat's Name:"));
+        console.log(perro.sayHello());
+        console.log(gato.sayHello());
+    });
+
 })();
