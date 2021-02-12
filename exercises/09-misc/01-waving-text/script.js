f42c6102c317d36c3c84c8a2ff63ed9f051c153e
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-12 12:26:16 +0100

    Add ppulsating text to 01-waving-text

--- exercises/09-misc/01-waving-text/script.js
+++ exercises/09-misc/01-waving-text/script.js
@@ -10,5 +10,23 @@
 // You will have time to focus on it later.
 
 (() => {
-
+    var i = 20;
+    var sense = "up";
+    target = document.getElementById("target");
+    setInterval(function(){
+        console.log(i);
+        if (i==26) {
+            sense = "down";
+        }
+        if (i==20) {
+            sense = "up";
+        }
+        if (sense == "up") {
+            i = i + 1;
+        }
+        if (sense == "down") {
+            i = i - 1;
+        }
+        target.setAttribute("style","font-size: "+i+"px;")
+    },120);
 })();
