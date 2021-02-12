79bd985b5e8239d0a4c4da72e43c2c6ee28b9745
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-12 11:30:42 +0100

    Nothing

--- exercises/09-misc/01-waving-text/script.js
+++ exercises/09-misc/01-waving-text/script.js
@@ -10,13 +10,5 @@
 // You will have time to focus on it later.
 
 (() => {
-    (() => {
-        srcImg = (img) => img.setAttribute("src", src);
-        hoverImg = (img) => img.setAttribute("src", hover);
-        var image = document.getElementsByTagName("IMG")[0];
-        src = image.getAttribute("src");
-        hover = image.getAttribute("data-hover");
-        image.setAttribute("onmouseout", "srcImg(this)");
-        image.setAttribute("onmouseover", "hoverImg(this)");
-    })();
+
 })();
